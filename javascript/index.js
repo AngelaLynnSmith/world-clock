31f122fce063354c016800697395cd86409ec729
Angela Lynn Smith <angiesmith1226@gmail.com>
2024-01-02 23:31:37 -0500

    added functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -21,7 +21,6 @@ sanJoseTimeElement.innerHTML = sanJoseTime.format(
 	"h:mm:ss [<small>]A[</small>]"
 );
 
-
 let limaElement = document.querySelector("#lima");
 let limaDateElement = limaElement.querySelector(".date");
 let limaTimeElement = limaElement.querySelector(".time");
@@ -44,5 +43,30 @@ santoriniTimeElement.innerHTML = santoriniTime.format(
 );
 }
 
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `  
+    <div class="city">
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.
+    format(
+    "A"
+    )}</small></div>
+</div>
+    `;
+}
+    
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
